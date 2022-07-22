51d5a4a2bbd12636da92e8c447957e0daa3b03ab
toyfat2309 <toyfat2309@gmail.com>
2022-07-22 11:21:08 -0700

    working on landing page

---
+++ src/components/ImageSlider.js
@@ -0,0 +1,40 @@
+import { Box } from '@mui/material'
+import { React, useState } from 'react'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import Landingpage from './Landingpage.css'
+
+const ImageSlider = ({ slides }) => {
+
+    const [currentIndex, setcurrentIndex] = useState(0)
+    const sliders = {
+        backgroundImage:'linear-gradient(to right,#fffdc2)'
+    }
+    const goPrevious = () => {
+        const isFirstSlide = currentIndex === 0
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1
+        setcurrentIndex(newIndex)
+    }
+    const goNext = () => {
+        const isLastSlide = currentIndex === slides.length - 1
+        const newIndex = isLastSlide ? 0 : currentIndex + 1
+        setcurrentIndex(newIndex)
+    }
+   
+    return (
+        <>
+            <Box sx={{ height: '100%', position: "relative" }}>
+                <ArrowForwardIosIcon sx={{position:'absolute',top:'50%',transform:'translate(0, -50%)',right:'35px',fontSize:'45px',color:'#fff',zIndex:1,cursor:'pointer'}} onClick={goNext}/>
+                <ArrowBackIosIcon sx={{position:'absolute',top:'50%',transform:'translate(0, -50%)',left:'35px',fontSize:'45px',color:'white',zIndex:1,cursor:'pointer'}} onClick={goPrevious}/>
+                <Box style={{ backgroundImage: `url(${slides[currentIndex].url})`, width: '100%', height: '100%', backgroundPosition: "right", backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}>
+                </Box>
+                <Box sx={{position:'absolute',top:0,left:0,height:'100%',width:'30%'}} className='tee'>
+                    lorem
+                </Box>
+            </Box>
+
+        </>
+    )
+}
+
+export default ImageSlider
